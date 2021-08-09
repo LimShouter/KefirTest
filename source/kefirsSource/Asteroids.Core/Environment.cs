75507ef2de96da5c31e63c0d512af887c23ceceb
limshouter <limshouter@gmail.com>
2021-08-09 21:18:42 +0300

    up: complete decompose environment data

--- source/kefirsSource/Asteroids.Core/Environment.cs
+++ source/kefirsSource/Asteroids.Core/Environment.cs
@@ -5,12 +5,14 @@ namespace Asteroids.Core
     public class Environment
     {
         public readonly CollectionContainer CollectionContainer = new CollectionContainer();
-        public readonly EnvironmentData EnvironmentData;
         public readonly PullCollection PullCollection = new PullCollection();
+        public readonly EnvironmentData EnvironmentData;
+        public readonly GameStaticFields GameStaticFields;
 
         public Environment(DescriptionCollection descriptions, ISaveModel saveModel)
         {
             EnvironmentData = new EnvironmentData(descriptions, saveModel);
+            GameStaticFields = new GameStaticFields(descriptions);
         }
     }
 }
